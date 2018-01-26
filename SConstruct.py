becd2a341df85c98be0f71e8ba2e374a3aa61ff9
Ivan Kalchev <ivan.kalchev@icloud.com>
2018-01-26 18:56:17 +0200

    Fix build for GCC (issue #1)

--- SConstruct.py
+++ SConstruct.py
@@ -1,55 +1,70 @@
+"""SConstruct file that builds:
+    - kpabe static lib
+    - main.cpp
+    - unittests
+"""
 import os
 
 CXXFLAGS = ["-std=gnu++14",]
 
 def getNativeEnv():
-   INCLUDES = [
-      "-I.",
-      "-I/usr/local/include",
-      "-I/usr/local/include/pbc",
-   ]
-   LIBPATH = [
-     "#",
-     "/usr/local/lib",
-   ]
-   LIBS = [
-     "pbc",
-     "gmp",
-     "mbedcrypto",
-     "m",
-   ]
+    """Get the environment.
+    """
+    INCLUDES = [
+        "-I.",
+        "-I/usr/local/include",
+        "-I/usr/local/include/pbc",
+    ]
+    LIBPATH = [
+      "#",
+      "/usr/local/lib",
+    ]
+    LIBS = [
+      "pbc",
+      "gmp",
+      "mbedcrypto",
+      "m",
+    ]
 
-   env = DefaultEnvironment(CXXFLAGS=CXXFLAGS + ["-Os"] + INCLUDES,
-                            LIBS=LIBS,
-                            LIBPATH=LIBPATH)
-   return env
+    env = DefaultEnvironment(CXXFLAGS=CXXFLAGS + ["-Os"] + INCLUDES,
+                             LIBS=LIBS,
+                             LIBPATH=LIBPATH)
+    return env
 
-def kpabeLib(env):
-   return env.StaticLibrary("kpabe", ["kpabe.cpp"])
+def getKpabeLib(env):
+    """Get target for kpabe static lib.
+    """
+    return env.StaticLibrary("kpabe", ["kpabe.cpp"])
 
-def registerTests(env):
-   BOOST_H = "/usr/local/include/boost"
-   BOOST_TEST_LIB = "boost_unit_test_framework"
+def getTestsTarget(env):
+    """Get test targets.
+    """
+    BOOST_H = "/usr/local/include/boost"
+    BOOST_TEST_LIB = "boost_unit_test_framework"
 
-   files = ["kpabe_test.cpp"]
-   testEnv = env.Clone()
-   testEnv["LIBS"].extend(["kpabe", BOOST_TEST_LIB])
-   testCases = [];
-   for f in files:
-      targetFile = os.path.join("#",
-                                str(f).split('.')[0].split(os.path.sep)[-1])
-      testCases.append(testEnv.Program(targetFile, [f]))
+    files = ["kpabe_test.cpp"]
+    testEnv = env.Clone()
+    testEnv["LIBS"].insert(0, ["kpabe", BOOST_TEST_LIB])
+    testCases = [];
+    for f in files:
+        targetFile = os.path.join("#",
+                                  str(f).split('.')[0].split(os.path.sep)[-1])
+        testCases.append(testEnv.Program(targetFile, [f]))
 
-   return testCases
+    return testCases
 
-def registerMain(env):
-  mainEnv = env.Clone()
-  mainEnv["LIBS"].append("kpabe")
-  return mainEnv.Program("main", "#main.cpp")
+def getMainTarget(env):
+    """Get main target.
+    """
+    mainEnv = env.Clone()
+    mainEnv["LIBS"].insert(0, "kpabe")
+    return mainEnv.Program("main", "#main.cpp")
 
-def collectNativeTargets(env):
-   targets = registerTests(env) + registerMain(env)
-   return targets
+def getAllTargets(env):
+    """Get all targets.
+    """
+    targets = getTestsTarget(env) + getMainTarget(env)
+    return targets
 
 env = getNativeEnv()
-env.Default(collectNativeTargets(env) + kpabeLib(env))
+env.Default(getAllTargets(env) + getKpabeLib(env))
